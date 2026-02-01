65f9477dbcb0247ebeb90217fb1cf29d4588e13f
Sakshyam-RT14 <pokhrelsakshyam8@gmail.com>
2026-02-01 18:03:05 +0545

    debugged

--- js/single-item.js
+++ js/single-item.js
@@ -1,21 +1,31 @@
-import { editCompleted, removeItem } from "./app.js";
-
+import { editCompleted,removeItem,setEditId } from "./app.js";
 export function createSingleItem(item) {
-    const div = document.createElement("div");
-    div.className = "single-item";
+  const div = document.createElement("div");
+  div.className = "single-item";
 
-    div.innerHTML = `
-        <input type="checkbox" ${item.completed ? "checked" : ""}>
-        <p style="text-decoration: ${item.completed ? "line-through" : "none"}">${item.name}</p>
-        <button class="btn icon-btn edit-btn" type="button"><i class="fa-regular fa-pen-to-square"></i></button>
-        <button class="btn icon-btn remove-btn" type="button"><i class="fa-regular fa-square-plus"></i></button><br>
-    `;
-    
-    const removeBtn = div.querySelector(".remove-btn");
-    removeBtn.addEventListener("click", () => removeItem(item.id));
+  div.innerHTML = `
+    <input type="checkbox" ${item.completed ? "checked" : ""} />
+    <p style="text-decoration: ${item.completed ? "line-through" : "none"}">
+      ${item.name}
+    </p>
+    <button class="btn icon-btn edit-btn" type="button">
+      <i class="fa-regular fa-pen-to-square"></i>
+    </button>
+    <button class="btn icon-btn remove-btn" type="button">
+      <i class="fa-regular fa-trash-can"></i>
+    </button>
+  `;
+   const checkbox = div.querySelector('input[type="checkbox"]');
+   checkbox.addEventListener("change", () => editCompleted(item.id));
 
-    const checkbox = div.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener("change", () => editCompleted(item.id));
+  const removeBtn = div.querySelector(".remove-btn");
+  removeBtn.addEventListener("click", () => removeItem(item.id));
 
-    return div;
+   const editBtn = div.querySelector(".edit-btn");
+  editBtn.addEventListener("click", () => setEditId(item.id));
+
+  return div;
+
 }
+
+
