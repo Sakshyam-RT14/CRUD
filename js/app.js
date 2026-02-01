256fc1b66382bbcb884a4ab8bb42717cd18a5506
Sakshyam-RT14 <pokhrelsakshyam8@gmail.com>
2026-02-01 16:34:26 +0545

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
 
+alert("its working")
 let items = groceryItems;
 
-// Render App
 function render() {
-  const app = document.getElementById("app");
-  app.innerHTML = "";
+    const app = document.getElementById("app");
+    app.innerHTML = "";
 
-  const itemsElement = createItems(items);
-  app.appendChild(itemsElement);
+    const itemsElement = createItems(items);
+    app.appendChild(itemsElement);
 }
 
-// Initialize App
+export function editCompleted(itemId) {
+    items = items.map((item) => {
+        if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+        }
+        return item;
+    });
+    render();
+}
+
+export function removeItem(itemId) {
+    items = items.filter((item) => item.id !== itemId);
+    render();
+    setTimeout(() => alert("Item Deleted Successfully!"), 0);
+}
+
 render();
