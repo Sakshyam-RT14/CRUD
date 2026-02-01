a9465032fe4d2a1e3ef2bb6fc56bf890125e27e4
Sakshyam-RT14 <pokhrelsakshyam8@gmail.com>
2026-02-01 16:36:52 +0545

    updated

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export const groceryItems = [
-  { id: "1", name: "milk", completed: true },
-  { id: "2", name: "bread", completed: true },
-  { id: "3", name: "eggs", completed: false },
-  { id: "4", name: "butter", completed: false },
+  { id: "1", name: "pasta", completed: true },
+  { id: "2", name: "meat", completed: true },
+  { id: "3", name: "oil", completed: false },
+  { id: "4", name: "salt", completed: false },
 ];
